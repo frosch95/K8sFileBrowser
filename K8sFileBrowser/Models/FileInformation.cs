f8cd7055e4ff72345b27999d1eb325cf03cd7552
Andreas Billmann <andreas@billmann.de>
2023-08-07 19:22:39 +0200

    Make double click working

--- K8sFileBrowser/Models/FileInformation.cs
+++ K8sFileBrowser/Models/FileInformation.cs
@@ -6,6 +6,7 @@ public class FileInformation
 {
     public string Parent { get; set; } = string.Empty;
     public FileType Type { get; set; } = FileType.File;
+    public string DisplayName => Parent.Length < 2 ? Name[Parent.Length..] : Name[( Parent.Length + 1)..];
     public string Name { get; set; } = string.Empty;
     public string Size { get; set; } = string.Empty;
     public DateTimeOffset Date { get; set; } = DateTimeOffset.MinValue;
